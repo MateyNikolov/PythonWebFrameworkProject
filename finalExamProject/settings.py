723a2b527c8485849956c500a11f272e8465b12e
Matey Nikolov <87667772+MateyNikolov@users.noreply.github.com>
2022-12-15 20:27:26 +0100

    added new models - rifle, agent, container. Made new functionality to add new Skin

--- finalExamProject/settings.py
+++ finalExamProject/settings.py
@@ -135,7 +135,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR, 'staticfiles']
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = [BASE_DIR, 'mediafiles']
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_REDIRECT_URL = reverse_lazy('home')
--- finalExamProject/urls.py
+++ finalExamProject/urls.py
@@ -1,11 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finalExamProject.common.urls')),
-    path('', include('finalExamProject.accounts.urls')),
-    path('', include('finalExamProject.profile_app.urls')),
-    path('', include('finalExamProject.skins.urls')),
-
-]
+    path('auth/', include('finalExamProject.accounts.urls')),
+    path('profile/', include('finalExamProject.profile_app.urls')),
+    path('skin/', include('finalExamProject.skins.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
