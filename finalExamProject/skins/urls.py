05ee38244cda40846cc32caa8167b4cbfceddd60
Matey Nikolov <87667772+MateyNikolov@users.noreply.github.com>
2022-12-17 01:17:10 +0100

    added new views, made view page for every skin, edited css

--- finalExamProject/skins/urls.py
+++ finalExamProject/skins/urls.py
@@ -1,14 +1,24 @@
 from django.urls import path, include
 
 from finalExamProject.skins.views import ShowAllSkinsView, ShowAllRiflesView, AddSkinView, AddRifleView, AddAgentView, \
-    AddContainerView
+    AddContainerView, ShowAllAgentsView, ShowAllContainersView, MySkinsView, ShowGunDetailsView, ShowAgentDetailsView, \
+    ShowContainerDetailsView
 
 urlpatterns = [
     path('all-skins/', ShowAllSkinsView.as_view(), name='all skins'),
     path('all-rifles/', ShowAllRiflesView.as_view(), name='all rifles'),
+    path('all-agents/', ShowAllAgentsView.as_view(), name='all agents'),
+    path('all-containers/', ShowAllContainersView.as_view(), name='all containers'),
+    path('my-skins/', MySkinsView.as_view(), name='my skins'),
     path('add-skin/', include([
         path('', AddSkinView.as_view(), name='add skin'),
         path('rifle/', AddRifleView.as_view(), name='add rifle'),
         path('agent/', AddAgentView.as_view(), name='add agent'),
         path('container/', AddContainerView.as_view(), name='add container'),
-    ]))]
+    ])),
+    path('details/', include([
+        path('gun/<int:pk>/', ShowGunDetailsView.as_view(), name='gun details'),
+        path('agent/<int:pk>/', ShowAgentDetailsView.as_view(), name='agent details'),
+        path('container/<int:pk>/', ShowContainerDetailsView.as_view(), name='container details'),
+    ]))
+    ]
--- finalExamProject/skins/views.py
+++ finalExamProject/skins/views.py
@@ -1,9 +1,8 @@
 from django import forms
-from django.shortcuts import render
 import django.views.generic as view
 from django.urls import reverse_lazy
 
-from finalExamProject.skins.models import Guns
+from finalExamProject.skins.models import Guns, Agent, Container
 
 
 class CreateRifleForm(forms.ModelForm):
@@ -17,6 +16,24 @@ class CreateRifleForm(forms.ModelForm):
         self.fields['description'].widget.attrs['style'] = 'resize: none;'
 
 
+class CreateAgentForm(forms.ModelForm):
+    class Meta:
+        model = Agent
+        fields = '__all__'
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['style'] = 'resize: none;'
+
+
+class CreateContainerForm(forms.ModelForm):
+    class Meta:
+        model = Container
+        fields = "__all__"
+        exclude = ('user',)
+
+
 class ShowAllSkinsView(view.TemplateView):
     template_name = 'skins/show_all_skins.html'
 
@@ -24,6 +41,16 @@ class ShowAllSkinsView(view.TemplateView):
 class ShowAllRiflesView(view.ListView):
     template_name = 'skins/show_all_rifles.html'
     model = Guns
+
+
+class ShowAllAgentsView(view.ListView):
+    template_name = 'skins/show_all_agents.html'
+    model = Agent
+
+
+class ShowAllContainersView(view.ListView):
+    template_name = 'skins/show_all_containers.html'
+    model = Container
 
 
 class AddSkinView(view.TemplateView):
@@ -42,8 +69,74 @@ class AddRifleView(view.CreateView):
 
 
 class AddAgentView(view.CreateView):
-    pass
+    template_name = 'skins/add_agent.html'
+    model = Agent
+    form_class = CreateAgentForm
+    success_url = reverse_lazy('all agents')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class AddContainerView(view.CreateView):
-    pass
+    template_name = 'skins/add_container.html'
+    model = Container
+    form_class = CreateContainerForm
+    success_url = reverse_lazy('all containers')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class MySkinsView(view.ListView):
+    template_name = 'skins/show_only_my_skins.html'
+    model = Guns
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['guns_objects'] = Guns.objects.filter(user_id=self.request.user.pk)
+        context['agent_objects'] = Agent.objects.filter(user_id=self.request.user.pk)
+        context['container_objects'] = Container.objects.filter(user_id=self.request.user.pk)
+        return context
+
+
+class ShowGunDetailsView(view.DetailView):
+    template_name = 'skins/gun_details.html'
+    model = Guns
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['obj'] = self.object
+        context['is_owner'] = self.object.user_id == self.request.user.pk
+        return context
+
+
+class ShowAgentDetailsView(view.DetailView):
+    template_name = 'skins/agent_details.html'
+    model = Agent
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['obj'] = self.object
+        context['is_owner'] = self.object.user_id == self.request.user.pk
+        if context['obj'].side == 't_side':
+            context['obj'].side_to_show = 'T Side'
+        else:
+            context['obj'].side_to_show = 'CT Side'
+        return context
+
+
+class ShowContainerDetailsView(view.DetailView):
+    template_name = 'skins/container_details.html'
+    model = Container
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['obj'] = self.object
+        context['is_owner'] = self.object.user_id == self.request.user.pk
+        type_to_split = context['obj'].type.split('_')
+        context['obj'].type_to_show = ' '.join(type_to_split)
+
+        return context
